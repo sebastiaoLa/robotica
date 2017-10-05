5243f8835cb1b61cc467482b2477dfde95e88ea0
sebastiaoLa <sebastiaolacerda@outlook.com>
2017-10-05 17:56:03 -0300

    ""

--- Robo/Robo/Robo.py
+++ Robo/Robo/Robo.py
@@ -3,7 +3,7 @@ from ev3dev.ev3 import *
 a = LargeMotor('outA')
 b = LargeMotor('outB')
 
-a.run_timed(time_sp = 60, speed_sp = 100),b.run_timed(time_sp = 60, speed_sp = 100)
+a.run_timed(time_sp = 3000, speed_sp = 360),b.run_timed(time_sp = 3000, speed_sp = 360)
 
 while a.is_running:
     pass
